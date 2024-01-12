9b05f7f7084861bab64b07646a75831bf3b2bba5
nooreddin emad <noorit2003@gmail.com>
2024-01-12 13:12:35 +0100

    anything

---
+++ streamFrontEnd/src/components/Root.jsx
@@ -0,0 +1,39 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./navbar/Navbar.js";
+import classes from "./Root.module.css"
+import ErrorBox from "./UI/ErrorBox/ErrorBox.js";
+import { useDispatch, useSelector } from "react-redux";
+import { errorActions } from "../store/error-slice.js";
+const Root=()=>{
+    const dispatch=useDispatch();
+    const showError=useSelector(state=>state.error.showError);
+    const divClickHandler=()=>{
+        dispatch(errorActions.hideError());
+    }
+    useEffect(()=>{
+        const storedData=localStorage.getItem('IsLoggedIn');
+        if(storedData==='1'){
+          const storedProfile=localStorage.getItem('uid');
+          try{
+
+          }
+          catch(error){
+
+          }
+        }      
+    },[])
+return(
+    <>
+    {showError &&<div className={classes.error}> <ErrorBox/>
+    </div>
+    }
+    {showError &&<div className={classes.backDrop} onClick={divClickHandler}></div>
+}
+    <div className={classes.firstContainer}>
+    
+    <main className={classes.main}><Outlet/></main> 
+    </div>
+    </>
+)
+}
+export default Root;
---
+++ streamFrontEnd/src/store/auth-slice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    loggedIn: false,
+    accountType: null,
+    uid: null,
+  },
+  reducers: {
+    logIn(state, action) {
+      const payload = action.payload;
+      state.loggedIn = true;
+      state.uid = payload.uid;
+      localStorage.setItem('IsLoggedIn','1');
+      localStorage.setItem('uid',JSON.stringify(state.uid));
+    },
+    logOut(state) {
+      state.loggedIn = false;
+      state.uid = null;
+      //auth.signOut();
+    },
+  },
+});
