2e156c736bc585fa7af3421b21720929d8d10cfa
shanshal <ahmedmahdii2003@gmail.com>
2024-01-12 16:17:29 +0300

    im not using vim

--- streamFrontEnd/src/App.jsx
+++ streamFrontEnd/src/App.jsx
@@ -1,26 +1,44 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { createBrowserRouter,RouterProvider, useLocation, useNavigate, useNavigation, useParams } from 'react-router-dom';
-
+import {
+    createBrowserRouter,
+    RouterProvider,
+    // useLocation,
+    // useNavigate,
+    // useNavigation,
+    // useParams
+} from 'react-router-dom';
+import Login from './components/login/Login';
+// import Register from './components/register/Register';
+import Root from './components/Root';
 import './App.css'
+import Home from './components/Home.jsx';
 
 function App() {
-  const router=createBrowserRouter([
-    {
-     path:"/",
-     element:<Root/>,
-     children:[
-       {
-         path:"/",
-         element:<MainPage/>
-       },]}]);
-  return (
-    <>
-  <RouterProvider router={router}>
-  
-  </RouterProvider>
-    </>
-  )
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <Root/>,
+            children: [
+                {
+                    path: "/",
+                    element: <Home/>
+                },
+                {
+                    path: "/Login",
+                    element: <Login/>
+                },
+                // {
+                //     path: "/Register",
+                //     element: <Register/>
+                // }
+
+                ]
+        }]);
+    return (
+        <>
+            <RouterProvider router={router}>
+            </RouterProvider>
+        </>
+    )
 }
 
 export default App
