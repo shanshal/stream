b3bd6fdfbaabf2908dc932027005030206e7bf9a
nooreddin emad <noorit2003@gmail.com>
2024-01-13 12:15:15 +0100

    hg

--- streamFrontEnd/src/components/NavBar.jsx
+++ streamFrontEnd/src/components/NavBar.jsx
@@ -17,7 +17,8 @@ function NavBar() {
                     <ul tabIndex={0}
                         className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         <li><Link to="/">Home</Link></li>
-                        <li><Link to={"/StreamPage"}>Profile</Link></li>
+                        <li><Link to={"/StreamPage"}>Stream</Link></li>
+                        <li><Link to={"/Profile"}>Profile</Link></li>
                         <li><Link to="/Movies">Movies</Link></li>
                     </ul>
 
