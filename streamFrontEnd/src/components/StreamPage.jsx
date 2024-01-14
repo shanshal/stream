04970972dc1f208fd1a2e6838993b61158013b16
YousifAljanabi <yosif.ph2003@gmail.com>
2024-01-14 09:42:43 +0300

    stream page

--- streamFrontEnd/src/components/StreamPage.jsx
+++ streamFrontEnd/src/components/StreamPage.jsx
@@ -1,8 +1,64 @@
 // cdimport React from 'react'
 import ReactPlayer from "react-player";
-import movie from "./movie.mp4"
+import {useAuth} from "../provider/authProvider.jsx";
+import {useState} from 'react';
+import axios from "axios";
+import React from "react";
 
 function StreamPage() {
+
+    const playerRef = React.useRef()
+
+    const [isPlaying, setIsPlaying] = useState(false)
+
+    const {token} = useAuth()
+    const roomName = 'test'
+
+    const [variable, setVariable] = useState([])
+
+
+    const partySocket = new WebSocket(
+        'ws://'
+        + '192.168.125.225:8000'
+        + '/ws/party/'
+        + 'roomName'
+        + '/'
+    );
+
+    partySocket.onmessage = function(e) {
+        const data = JSON.parse(e.data);
+        console.log(data)
+        if(data.type === 'chat_message'){
+            console.log('dw')
+            setVariable(() => [...variable, {
+                from: data.from,
+                message: data.message,
+                time: data.time,
+            }])
+        }
+        console.log(data)
+        if(data.type === 'video_state'){
+            setIsPlaying(data.state)
+            // playerRef.current?.seekTo(playerRef?.current.getCurrentTime(), 'seconds')
+            // playerRef.current.playing = true;
+        }
+        if(data.type === 'time_sync'){
+            console.log(data.time)
+            playerRef.current?.seekTo(data.time, "seconds")
+        }
+
+
+    };
+
+
+
+    partySocket.onclose = function(e) {
+        console.error('Chat socket closed unexpectedly');
+    };
+
+
+
+
 
     return (
         <div>
@@ -10,8 +66,22 @@ function StreamPage() {
                 <div className={"screenSide lg:w-2/3 m-4 flex flex-col justify-evenly items-start"}>
                     <div className={"m-2 rounded-md w-full"}>
                         <ReactPlayer
-                            url={movie}
-                            playing={true}
+                            url={'https://www.youtube.com/embed/O0Py7S-qLRI?si=myP53u7vZeBvOMJt'}
+                            playing={isPlaying}
+                            ref={playerRef}
+                            onPlay={()=> {
+                                partySocket.send(JSON.stringify({
+                                    state: true,
+                                    type: 'video_state',
+                                }));
+                            }}
+                            onPause={()=> {
+                                partySocket.send(JSON.stringify({
+                                    state: false,
+                                    type: 'video_state',
+                                }));
+                            }}
+
                             controls={true}
                             width={"100%"}
                             height={"100%"}
@@ -44,28 +114,60 @@ function StreamPage() {
                 <div className={"chatSide lg:w-1/3 m-2"}>
                     <div
                         className={"chatBox bg-base-300 border-2 border-info rounded-md flex flex-col overflow-y-auto h-3/4 max-h-3/4"}>
-                        <div className={"chatComponent flex-col"}>
-                            <div className="chat chat-start">
-                                <div className="chat-header">
-                                    Obi-Wan Kenobi
-                                    <time className="text-xs opacity-50">2 hours ago</time>
-                                </div>
-                                <div className="chat-bubble">You were the Chosen One!</div>
-                            </div>
-                            <div className="chat chat-end">
-                                <div className="chat-header">
-                                    Anakin
-                                    <time className="text-xs opacity-50">2 hours ago</time>
-                                </div>
-                                <div className="chat-bubble">I hate you</div>
-                            </div>
+                        <div className={"chatComponent flex-col"} id={'chatBox'}>
+                            {variable.map((variable) => {
+                                return (
+                                    <div className="chat chat-start" key={variable.time}>
+                                        <div className="chat-header">
+                                            {variable.from}
+                                            <time className="text-xs opacity-50">{variable.time}</time>
+                                        </div>
+                                        <div className="chat-bubble">{variable.message}</div>
+                                    </div>
+                                )
+                            })}
+
                         </div>
                     </div>
                     <div
                         className={"bg-base-300 chatInput self-end border-2 border-info rounded-md w-full flex justify-center items-center "}>
                         <input type="text" placeholder="Type here"
-                               className="input input-bordered input-info w-full max-w-xs"/>
-                        <button className="m-4 btn btn-warning btn-sm sm:btn-sm md:btn-md lg:btn-md ">Send</button>
+                               className="input input-bordered input-info w-full max-w-xs"
+                                id={'message_input'}
+                        />
+                        <button className="m-4 btn btn-warning btn-sm sm:btn-sm md:btn-md lg:btn-md " id={'send_message'}
+                            onClick={
+                                async function() {
+                                    const messageInputDom = document.querySelector('#message_input');
+                                    const message = messageInputDom.value;
+                                    try{
+                                        axios.get('http://192.168.125.225:8000/api/account/check', {
+                                            'headers': {
+                                                'Authorization': `Bearer ${token}`
+                                            }
+                                        })
+                                            .then(function (response){
+                                                partySocket.send(JSON.stringify({
+
+                                                    message: message,
+                                                    type: 'chat_message',
+                                                    from: response.data.email,
+                                                    time:  `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}  `,
+
+                                                }));
+                                            })
+
+                                    }
+                                    catch(error){
+                                        console.log(error)
+
+                                    }
+
+                                    messageInputDom.value = '';
+
+                                }
+                            }
+                        >Send</button>
 
                     </div>
                 </div>
